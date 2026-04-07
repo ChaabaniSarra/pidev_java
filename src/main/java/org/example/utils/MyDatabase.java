916d9d9f75e234c41fd899699cd24d25e751ef19
maryem <saadamaryem112@gmail.com>
2026-04-07 19:59:09 +0100

    crudmaryem

--- src/main/java/org/example/utils/MyDatabase.java
+++ src/main/java/org/example/utils/MyDatabase.java
@@ -1,31 +1,34 @@
 package org.example.utils;
+
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyDatabase {
-    private final String url="jdbc:mysql://localhost:3306/esports";
-    private final String user="root";
-    private final String password="";
-    private Connection connection;
     private static MyDatabase instance;
+    private Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/esport-db";
+    private static final String USER = "root";
+    private static final String PASSWORD = ""; // ton mot de passe
+
     private MyDatabase() {
         try {
-            connection= DriverManager.getConnection(url,user,password);
-            System.out.println("connected to database");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connexion réussie ");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            System.out.println(" Erreur connexion : " + e.getMessage());
         }
     }
-    public Connection getConnection () {
-        return connection;
-    }
+
     public static MyDatabase getInstance() {
-        if(instance==null){
-            instance= new MyDatabase();
+        if (instance == null) {
+            instance = new MyDatabase();
         }
         return instance;
     }
 
+    public Connection getConnection() {
+        return connection;
+    }
 }
